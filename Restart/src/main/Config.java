d2345c740ce6d8bfbbc2073b3cc0e0646ed6bf12
grizly1326 <daniel.maly@hotmail.com>
2016-02-23 19:23:35 +0100

    23.2.2016 Commit.

---
+++ Restart/src/main/Config.java
@@ -0,0 +1,11 @@
+package main;
+
+import javax.swing.JFrame;
+
+public class Config {
+	public static boolean run=false;
+	public static String gateway="10.0.0.1";
+	public static int timeout=2000;
+	public static String logPath="";
+	public static JFrame frame=new JFrame();
+}
